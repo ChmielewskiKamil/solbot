511106b1c6c8cbc4833d24b7c1ae43b873506d77
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-03-02 14:23:09 +0100

    👷 Experiment lexing contracts

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"solparsor/lexer"
+	"solparsor/token"
+)
+
 func main() {
-	println("Solidity Parser go brrrrr....")
+	input := `
+    Contract Vault {
+        uint256 x;
+        x = 5;
+    }
+    `
+	lexer := lexer.Lex(input)
+
+	for {
+		tkn := lexer.NextToken()
+		fmt.Printf("Token: %s, at position: %d, with type: %d\n", tkn.String(), tkn.Pos, tkn.Type)
+
+		if tkn.Type == token.EOF || tkn.Type == token.ERROR {
+			break
+		}
+	}
 }
