3007fd645a686fb4f816b07978cdc04ee262d7a2
Kamil Chmielewski <45183584+ChmielewskiKamil@users.noreply.github.com>
2024-09-08 00:55:24 +0200

    👷 Define integer, boolean objects

---
+++ object/object.go
@@ -0,0 +1,32 @@
+// object or "value" system used to evaluate the AST nodes.
+package object
+
+import (
+	"fmt"
+	"math/big"
+)
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value big.Int
+}
+
+func (i *Integer) Inspect() string  { return i.Value.String() }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
