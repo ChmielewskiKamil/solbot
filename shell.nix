28775d2b80d5b456f3b1f6f87fd1d655054e545e
Kamil Chmielewski <45183584+ChmielewskiKamil@users.noreply.github.com>
2025-08-01 19:18:08 +0200

    Add dev shell with Go tooling

---
+++ shell.nix
@@ -0,0 +1,15 @@
+# Minimal Go development environment with latest go version and delve debugger.
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    go
+    delve
+  ];
+
+  # Isolate project specific dependencies from the rest of the system.
+  shellHook = ''
+    export GOPATH=$(pwd)/.go
+    export GOBIN=$GOPATH/bin
+    export PATH=$GOBIN:$PATH
+  '';
+}
