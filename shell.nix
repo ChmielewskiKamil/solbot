95f81866064e72fc1f7e752a5d976c030ab00ce9
Kamil Chmielewski <45183584+ChmielewskiKamil@users.noreply.github.com>
2025-08-08 23:08:32 +0200

    chore: Add scripts to shell.nix

--- shell.nix
+++ shell.nix
@@ -7,10 +7,30 @@ pkgs.mkShell {
     goreleaser
   ];
 
-  # Isolate project specific dependencies from the rest of the system.
   shellHook = ''
+    # Isolate project specific dependencies from the rest of the system.
     export GOPATH=$(pwd)/.go
     export GOBIN=$GOPATH/bin
     export PATH=$GOBIN:$PATH
+
+    ### Go Releaser Logic ###
+
+    export GITHUB_TOKEN=$GITHUB_TOKEN
+
+    release() {
+      if [ -z "$GITHUB_TOKEN" ]; then
+        echo "Error: GITHUB_TOKEN is not set."
+        echo "Please source your .env file or export it."
+        return 1
+      fi
+      
+      goreleaser release --clean
+    }
+
+    ### Test ###
+
+    test() {
+      go test ./...
+    }
   '';
 }
